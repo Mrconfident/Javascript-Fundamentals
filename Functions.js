12cec80155bb42c456eaf2374d935d6348b4b693
Tarun Lunia <tarunlunia1998@gmail.com>
2023-11-24 15:44:03 +0530

    Added Few Things

--- Functions.js
+++ Functions.js
@@ -1,25 +1,57 @@
+// This is Function Statement
 function greet () {
+  const b = 'Hi'
+  console.log(this)
   console.log('Hii')
 }
-
-// This is Function Statement
 
 greet.lang = 'en'
 
 console.log(greet.lang)
+greet()
 
 
-// Function Statements and Function Expresion
+// anonymous() {
 
-// anonymous()  // Error 
+// } Error 
 
+// The above one is Function Expresion
 const anonymous = function () {
   console.log('Expression')
 }
 
-console.log(anonymous)
+anonymous()
 
-// The above one is Function Expresion
+
+// Arrow function
+
+// arrow functions cannot be declared; they can only be expressed.
+
+const arrow = () => {
+  const a = 'Hii'
+  console.log(this)
+}
+
+arrow()
+
+
+const obj = {
+  name: 'Tarun',
+  class: 9,
+  print: function () {
+    console.log(this)
+  }
+}
+
+obj.print()
+
+/*
+Arrow functions don't have their own bindings to this, arguments, or super, and should not be used as methods.
+Arrow functions cannot be used as constructors. Calling them with new throws a TypeError. They also don't have access to the new.target keyword.
+Arrow functions cannot use yield within their body and cannot be created as generator functions. */
+
+
+
 
 
 
